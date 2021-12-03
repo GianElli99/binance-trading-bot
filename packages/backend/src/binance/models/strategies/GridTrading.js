710a5a5d215eb0f9f6077467787819e6ca9680db
Gianfranco Elli <gianelli99@hotmail.com>
2021-12-03 18:48:53 -0300

    strategy implementation created, i think it works

--- packages/backend/src/binance/models/strategies/GridTrading.js
+++ packages/backend/src/binance/models/strategies/GridTrading.js
@@ -6,28 +6,124 @@ class GridTrading {
   busd_free_amount;
   busd_locked_amount;
 
-  static grid_gap = 0.0125;
-  static grid_trailing_stop_gap = 0.0025;
+  grid_gap = 0.0005; //0.0125;
+  grid_trailing_stop_gap = 0.0004; //0.0025;
   calculate_difference = 0.0005;
 
   initial_price = undefined;
+  last_price = undefined;
+
+  sellOrder = undefined;
+  buyOrder = undefined;
   constructor(name = 'Grid Trading') {
     this.name = name;
   }
   run(data) {
     if (!this.initial_price) {
       this.initial_price = Number(data.c);
+      this.last_price = Number(data.c);
+      console.log('Initial price', this.initial_price);
       return;
     }
     let close_price = Number(data.c);
     if (
-      Math.abs(close_price - this.initial_price) >
-      this.initial_price * this.calculate_difference
+      Math.abs(close_price - this.last_price) >
+      this.last_price * this.calculate_difference
     ) {
-      console.log(this.initial_price, ' Initial ', close_price, ' Close');
-      this.initial_price = close_price;
+      this.last_price = close_price;
+      console.log(
+        this.initial_price,
+        ' Initial',
+        this.last_price,
+        ' Last calculated',
+      );
+      this.placeOrder();
     }
   }
+  placeOrder() {
+    if (this.last_price >= this.initial_price) {
+      this.sell();
+    } else {
+      this.buy();
+    }
+  }
+  buy() {
+    console.log(this.buyOrder ? 'existe buy order' : 'no existe buy');
+    if (this.buyOrder) {
+      //hay orden
+      if (this.last_price < this.buyOrder.placedAt) {
+        //hay que bajarla
+        console.log(
+          'Trailing buy order modified to ',
+          this.last_price + this.last_price * this.grid_trailing_stop_gap,
+        );
+        this.buyOrder = {
+          placedAt: this.last_price,
+          price:
+            this.last_price + this.last_price * this.grid_trailing_stop_gap,
+        };
+        console.log(`this.buyOrder `, this.buyOrder);
+      }
+    } else {
+      //no hay orden
+      if (this.last_price < this.initial_price * (1 - this.grid_gap)) {
+        //supera el trigger, hay que crearla entonces
+        console.log(
+          'Place new buy order at ',
+          this.last_price + this.last_price * this.grid_trailing_stop_gap,
+        );
+        this.buyOrder = {
+          placedAt: this.last_price,
+          price:
+            this.last_price + this.last_price * this.grid_trailing_stop_gap,
+        };
+        console.log(`this.buyOrder `, this.buyOrder);
+      }
+    }
+  }
+
+  sell() {
+    console.log(this.sellOrder ? 'existe sell order' : 'no existe sell');
+
+    if (this.sellOrder) {
+      //hay orden
+      if (this.last_price > this.sellOrder.placedAt) {
+        //hay que elevarla
+        console.log(
+          'Trailing sell order modified to ',
+          (
+            this.last_price -
+            this.last_price * this.grid_trailing_stop_gap
+          ).toString(),
+        );
+        this.sellOrder = {
+          placedAt: this.last_price,
+          price:
+            this.last_price - this.last_price * this.grid_trailing_stop_gap,
+        };
+        console.log(`this.sellOrder `, this.sellOrder);
+      }
+    } else {
+      //no hay orden
+      if (this.last_price > this.initial_price * (1 + this.grid_gap)) {
+        //supera el trigger, hay que crearla entonces
+        console.log(
+          'Place new sell order at ',
+          (
+            this.last_price -
+            this.last_price * this.grid_trailing_stop_gap
+          ).toString(),
+        );
+        this.sellOrder = {
+          placedAt: this.last_price,
+          price:
+            this.last_price - this.last_price * this.grid_trailing_stop_gap,
+        };
+        console.log(`this.sellOrder `, this.sellOrder);
+      }
+    }
+  }
+
   async setInitialState() {
     const res = await this.account.accountInfo();
     this.btc_free_amount = Number(
