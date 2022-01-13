f236be822b1feb80c2e5cb6206057520159318df
Gianfranco Elli <gianelli99@hotmail.com>
2022-01-12 22:48:05 -0300

    no more start and stop from api

--- packages/backend/src/web-api/index.js
+++ packages/backend/src/web-api/index.js
@@ -7,17 +7,17 @@ api.use(cors());
 
 const msgBroker = new MessageBroker();
 
-api.post('/stop', async (req, res) => {
-  console.log('WEB API: STOP');
-  msgBroker.emit('stop');
-  res.status(200).json({});
-});
+// api.post('/stop', async (req, res) => {
+//   console.log('WEB API: STOP');
+//   msgBroker.emit('stop');
+//   res.status(200).json({});
+// });
 
-api.post('/start', async (req, res) => {
-  console.log('WEB API: START');
-  msgBroker.emit('start');
-  res.status(200).json({});
-});
+// api.post('/start', async (req, res) => {
+//   console.log('WEB API: START');
+//   msgBroker.emit('start');
+//   res.status(200).json({});
+// });
 api.get('/', async (req, res) => {
   console.log('WEB API: PREVENT SLEEPING');
   res.status(200).json({ msg: 'Preventing app from sleeping' });
